95e10faf82ff9f0d42f73504bf19d30f2ea80eef
alfonsinamoris <105081595+alfonsinamoris@users.noreply.github.com>
2023-11-14 21:28:32 -0300

    cambios

--- tp4/script.js
+++ tp4/script.js
@@ -7,11 +7,15 @@ var navbar = document.querySelector(".header");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
+var logo = document.getElementById("logoNav");
+
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    logo.style.display="none";
   } else {
     navbar.classList.remove("sticky");
+    logo.style.display="grid";
   }
 }
