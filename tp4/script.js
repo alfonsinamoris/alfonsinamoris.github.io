64cc1136f095608384b908798efa36ee6e452bef
alfonsinamoris <105081595+alfonsinamoris@users.noreply.github.com>
2023-11-21 16:44:29 -0300

    primer parallax

--- tp4/script.js
+++ tp4/script.js
@@ -15,13 +15,13 @@ function myFunction() {
     logo.style.display="none";
   }
 }
-/*s
+
 //parallax
 let logoPag1 = document.getElementById('logoPag1');
 let capa1 = document.getElementById('capa1');
-let capa2 = document.getElementById('capa2');
-let capa3 = document.getElementById('capa3');
-let capa4 = document.getElementById('capa4');
+//let capa2 = document.getElementById('capa2');
+//let capa3 = document.getElementById('capa3');
+//let capa4 = document.getElementById('capa4');
 let capa5 = document.getElementById('capa5');
 let capa6 = document.getElementById('capa6');
 let capa7 = document.getElementById('capa7');
@@ -29,18 +29,22 @@ let capa8 = document.getElementById('capa8');
 let capa9 = document.getElementById('capa9');
 let duende = document.getElementById('duende');
 
+//agarra la posicion inicial de las capas para moverlas a partir de esa
+let intialBottomPosition7 = parseFloat(getComputedStyle(capa7).bottom);
+let intialRightPosition7 = parseFloat(getComputedStyle(capa7).right);
+let intialLeftPosition5 = parseFloat(getComputedStyle(capa5).left);
+let intialBottomPosition6 = parseFloat(getComputedStyle(capa6).bottom);
+
+
 window.addEventListener('scroll',()=>{
   let value = window.scrollY;
-  //le agrega o resta px o porcentajes del inicial a medida q se scrollea
-  capa7.style.bottom = value * 1.5 + 'px';
-  capa7.style.right = value * -0.4 + 'px';
-  capa3.style.bottom = value * -0.1 +'%';
-  capa2.style.bottom = value * -0.1 +'%';
-  capa4.style.bottom = value * -1 +'px';
-  capa5.style.left = value * -0.4 +'px';
-  logoPag1.style.top = value *-1 +'px';
-  capa6.style.bottom = value * 1+'px';
+  //le agrega o resta px o porcentajes del inicial a medida q se scrollea y le suma el scroll en y
+  capa7.style.bottom = intialBottomPosition7 + value * 0.5 + 'px';
+  capa7.style.right = intialRightPosition7 + value * -0.5 + 'px';
+  capa5.style.left = intialLeftPosition5 + value * -0.4 +'px';
+  logoPag1.style.top = value *-0.5 +'px';
+  capa6.style.bottom =intialBottomPosition6 + value * 0.5+'px';
 
 
   //duende.style.top = value * -0.5 +'px';
-});*/
+});
