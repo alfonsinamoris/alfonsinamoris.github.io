ac5e917e9021fc8138efd1dba2aefa63727ee355
alfonsinamoris <105081595+alfonsinamoris@users.noreply.github.com>
2023-11-23 18:12:43 -0300

    cambios

---
+++ tp4/seccion6.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+let spiderRosa = document.getElementById("spiderRosa");
+let spiderNegro = document.getElementById("spiderNegro").addEventListener("mouseover", spiderNegro);
+let spiderRojo = document.getElementById("spiderRojo").addEventListener("mouseover", spiderRojo);
+let contenedor = document.querySelector("#tresSpiders")
+
+spiderRosa.onmouseover = function(){
+
+};
+
+});
