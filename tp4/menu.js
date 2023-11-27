bbb2e50865219af2e3fb029718b31cf4d502800e
alfonsinamoris <105081595+alfonsinamoris@users.noreply.github.com>
2023-11-27 18:21:37 -0300

    cmbios

--- tp4/menu.js
+++ tp4/menu.js
@@ -1,23 +1,31 @@
 let menu = document.querySelector(".navigation");
 let abierto = false;
-function toggleMenu(){
-    if(abierto===false){
-    menu.style.display = "flex";
-    var listItems = document.querySelectorAll('.navigation ul li');
-
-    function showItems(index) {
+function toggleMenu() {
+    if (abierto==false) {
+      // Si el menú no está abierto, ábrelo
+      menu.style.display = "flex";
+      var listItems = document.querySelectorAll('.navigation ul li');
+      function showItems(index) {
         if (index < listItems.length) {
-            listItems[index].classList.add('visible');
-            setTimeout(function() {
-                showItems(index + 1);
-            }, 300);
+          listItems[index].classList.add('visible');
+          setTimeout(function () {
+            showItems(index + 1);
+          }, 300);
         }
-    }
+      }
+      showItems(0);
+      abierto = true;
+    } else if(abierto==true){
+      // Si el menú está abierto, ciérralo
+      menu.style.display = "none";
 
-    showItems(0);
-    abierto=true;
+      // También puedes agregar lógica para ocultar los elementos con un efecto si lo deseas
+      var listItems = document.querySelectorAll('.navigation ul li');
+      listItems.forEach(item => {
+        item.classList.remove('visible');
+      });
+
+      abierto = false;
     }
-    else {
-       
-    }
+  
 }
--- tp4/seccion5.js
+++ tp4/seccion5.js
@@ -1,15 +1,15 @@
 //parallax hover
 document.addEventListener("mousemove", parallax);
-const elem = document.querySelector("#aranaNegro");
-function parallax(e){
-  let _w = window.innerWidth/2;
-      let _h = window.innerHeight/2;
-      let _mouseX = e.clientX;
-      let _mouseY = e.clientY;
-      let _depth1 = `${50 - (_mouseX - _w) * 1}px ${50 - (_mouseY - _h) * 1}px`;
-      let _depth2 = `${50 - (_mouseX - _w) * 1}px ${50 - (_mouseY - _h) * 1}px`;
-      let _depth3 = `${50 - (_mouseX - _w) * 1}px ${50 - (_mouseY - _h) * 1}px`;
-      let x = `${_depth3}, ${_depth2}, ${_depth1}`;
-      //console.log(x);
-      elem.style.backgroundPosition = x;
-}
+    const elem = document.querySelector("#aranaNegro");
+
+    function parallax(e){
+        let _w = window.innerWidth / 2;
+        let _h = window.innerHeight / 2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.2}px ${50 - (_mouseY - _h) * 0.2}px`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.2}px ${50 - (_mouseY - _h) * 0.2}px`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.2}px ${50 - (_mouseY - _h) * 0.2}px`;
+        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+        elem.style.backgroundPosition = x;
+    }
